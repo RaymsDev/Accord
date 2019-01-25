79bc47aa06b1d360ccb473f5faa5883c6e76a440
Leidanium <alexsam.quentin@gmail.com>
2019-01-25 14:52:13 +0100

    Add firebase env const

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDhyQwysEpwjMg9oqQGELvKP3THSS_Tgww',
+    projectId: 'accord-204f9',
+    storageBucket: 'accord-204f9.appspot.com'
+  }
 };
 
 /*
