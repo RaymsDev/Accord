6f8b94a81da281ea140f17c9b83fe5bf529685fa
Rayms <rayms.dev@gmail.com>
2019-03-01 17:48:25 +0100

    Add build pipeline

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,23 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-import { Platform } from "@ionic/angular";
-import { SplashScreen } from "@ionic-native/splash-screen/ngx";
-import { StatusBar } from "@ionic-native/status-bar/ngx";
-import { RoomService } from "./services/room.service";
+import { Platform, ToastController } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { RoomService } from './services/room.service';
+import { Firebase } from '@ionic-native/firebase/ngx';
 
 const pages = [
   {
-    title: "Home",
-    url: "/home",
-    icon: "home"
+    title: 'Home',
+    url: '/home',
+    icon: 'home'
   }
 ];
 
 @Component({
-  selector: "app-root",
-  templateUrl: "app.component.html",
-  styleUrls: ["./app.component.scss"]
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   public appPages = pages;
@@ -25,7 +26,9 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private roomService: RoomService
+    private roomService: RoomService,
+    private firebase: Firebase,
+    private toaster: ToastController
   ) {
     this.initializeApp();
   }
@@ -34,6 +37,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.initNotifications();
       this.initRooms();
     });
   }
@@ -46,10 +50,30 @@ export class AppComponent {
         .forEach(r => {
           this.appPages.push({
             title: r.name,
-            url: "/room/" + r.id,
+            url: '/room/' + r.id,
             icon: r.icon
           });
         });
     });
+  }
+
+  private initNotifications() {
+    if (this.platform.is('mobile')) {
+      this.firebase.subscribe('all');
+
+      this.firebase.onNotificationOpen()
+        .subscribe(async response => {
+          if (response.tap) {
+
+          } else {
+            const toast = await this.toaster.create({
+              message: response.body,
+              duration: 3000
+            });
+
+            toast.present();
+          }
+        });
+    }
   }
 }
--- src/app/room/room.page.ts
+++ src/app/room/room.page.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { IRoom } from "../models/IRoom";
-import { RoomService } from "../services/room.service";
-import { Observable } from "rxjs/internal/Observable";
-import { IMessage } from "../models/IMessage";
-import { UserService } from "../services/user.service";
-import { IUser } from "../models/IUser";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IRoom } from '../models/IRoom';
+import { RoomService } from '../services/room.service';
+import { Observable } from 'rxjs/internal/Observable';
+import { IMessage } from '../models/IMessage';
+import { UserService } from '../services/user.service';
+import { IUser } from '../models/IUser';
 
 @Component({
-  selector: "app-room",
-  templateUrl: "./room.page.html",
-  styleUrls: ["./room.page.scss"]
+  selector: 'app-room',
+  templateUrl: './room.page.html',
+  styleUrls: ['./room.page.scss']
 })
 export class RoomPage implements OnInit {
-  @ViewChild("content") content;
+  @ViewChild('content') content;
   public CurrentUser: IUser;
   public Room$: Observable<IRoom>;
   public newMessage: string;
@@ -35,13 +35,13 @@ export class RoomPage implements OnInit {
   }
 
   private initRoom() {
-    const roomId = this.route.snapshot.paramMap.get("id");
+    const roomId = this.route.snapshot.paramMap.get('id');
     this.Room$ = this.roomService.JoinUser(roomId);
   }
 
   public SendMessage(roomId: string) {
     this.roomService.SendMessage(this.CurrentUser, roomId, this.newMessage);
-    this.newMessage = "";
+    this.newMessage = '';
   }
 
   public trackByCreated(i, message: IMessage) {
