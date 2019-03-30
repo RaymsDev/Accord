d0ca377ec053735f5ad9bb38df38b85f8408936b
Leidanium <alexsam.quentin@gmail.com>
2019-03-30 22:15:31 +0100

    Fix trouble module import user-item

---
+++ src/app/components/user-item/user-item.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserItemComponent } from './user-item.component';
+
+@NgModule({
+    imports: [],
+    declarations: [
+        UserItemComponent
+    ],
+    exports: [
+        UserItemComponent
+    ]
+    // declarations: [
+    //     UserItemComponent
+    // ]
+})
+export class UserItemModule { }
